256db286f82a0d937a23b6ed511144e6ccd30604
DanaStreltsova <streltsovabeauty@gmail.com>
2024-11-18 23:10:23 +0100

    'update'

--- src/test/java/ait/HomeWork.java
+++ src/test/java/ait/HomeWork.java
@@ -1,6 +1,8 @@
 package ait;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -26,4 +28,36 @@ public class HomeWork {
     public void tearDawn(){
         driver.close();
     }
+
+    @Test
+    public void findElementBySimpleLocator(){
+
+        driver.findElement(By.id("dialog-notifications-success"));
+        driver.findElement(By.className("header-logo"));
+        driver.findElement(By.className("header-menu"));
+        driver.findElement(By.className("top-menu"));
+        driver.findElement(By.className("header-links-wrapper"));
+        driver.findElement(By.linkText("Digital downloads"));
+        driver.findElement(By.partialLinkText("Books"));
+        driver.findElement(By.partialLinkText("Computers"));
+        driver.findElement(By.partialLinkText("Electronics"));
+        WebElement p = driver.findElement(By.tagName("p"));
+    }
+
+    @Test
+    public void findElementByCssSelector(){
+        driver.findElement(By.cssSelector("h3"));
+        driver.findElement(By.cssSelector("#nivo-slider"));
+        driver.findElement(By.cssSelector(".header"));
+        driver.findElement(By.cssSelector(".header-links"));
+        driver.findElement(By.cssSelector(".cart-qty"));
+        driver.findElement(By.cssSelector("[href='/cart']"));
+        driver.findElement(By.cssSelector("[href^='login']"));
+        driver.findElement(By.cssSelector("[href^='/wishlist']"));
+        driver.findElement(By.cssSelector("[href$='search']"));
+
+    }
+
+
+
 }
